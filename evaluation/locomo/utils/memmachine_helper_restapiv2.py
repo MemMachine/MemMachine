1e198bd29f8196ecf6872f7df34e0ed233c46021
Tom Wong <tom.wong@memverge.com>
2026-01-06 13:59:24 -0800

    workaround poorly selected ruff checks
    Signed-off-by: Tom Wong <tom.wong@memverge.com>

--- evaluation/locomo/utils/memmachine_helper_restapiv2.py
+++ evaluation/locomo/utils/memmachine_helper_restapiv2.py
@@ -2,7 +2,7 @@
 # There are 2 copies of this file, please keep them both the same
 # 1. memmachine-test/benchmark/mem0_locomo/tests/memmachine
 # 2. memmachine-test/benchmark/mem0_locomo/tests/mods/MemMachine/evaluation/locomo/utils
-# ruff: noqa: PTH118, SIM108, G004, C901, SIM105, SIM102, ASYNC109
+# ruff: noqa: PTH118, SIM108, G004, C901, SIM105, SIM102, ASYNC109, SIM117
 
 import copy
 import os
@@ -547,7 +547,7 @@ class MemmachineHelperRestapiv2(MemmachineHelperBase):
                     raise AssertionError(
                         f"ERROR: status_code={resp.status} reason={resp.reason}"
                     )
-                resp_text = await resp.text(encoding='utf-8')
+                resp_text = await resp.text(encoding="utf-8")
                 self.log.debug(f"text={resp_text}")
 
         data = await resp.json()
